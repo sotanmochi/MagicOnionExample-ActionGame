b48ad1996013a91520255614d377800638b1b39f
sotanmochi <stmczk@gmail.com>
2019-06-24 13:15:35 +0900

    Update main scene

---
+++ MagicOnionActionGame.Unity/Assets/MagicOnionActionGame/Utilities/CameraControll.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class CameraControll : MonoBehaviour
+{
+	public GameObject TargetObj;
+	private Vector3 previousTargetPos;
+
+	void Start()
+    {
+        previousTargetPos = TargetObj.transform.position;
+	}
+
+    void Update()
+    {
+        Vector3 targetPosDiff = TargetObj.transform.position - previousTargetPos;
+		transform.position = transform.position + targetPosDiff;
+		previousTargetPos = TargetObj.transform.position;
+        
+        if (Input.GetMouseButton(0))
+        {
+			float mouseInputX = Input.GetAxis("Mouse X");
+			transform.RotateAround(TargetObj.transform.position, Vector3.up, mouseInputX * Time.deltaTime * 180f);
+		}
+    }
+}
